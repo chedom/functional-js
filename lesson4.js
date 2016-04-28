3379896b08fbb9814ed0db4f588a3e68ed85a879
Vasyl Domanchuk <domanchukits@gmail.com>
2016-04-28 22:47:59 +0300

    done lesson4

---
+++ lesson4.js
@@ -0,0 +1,11 @@
+function getShortMessages(messages) {
+  // SOLUTION GOES HERE
+  return messages.filter(function(val) {
+  	return val.message.length < 50;
+  })
+  .map(function(val) {
+  	return val.message;
+  });
+}
+
+module.exports = getShortMessages
